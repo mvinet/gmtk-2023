91d427e77fdc149c716c88eb264e7985cca82b97
imberbus <littlelepetit@gmail.com>
2023-07-08 14:05:14 +0200

    Add rarity to MouseDefinition

--- Assets/Scripts/Entities/Definitions/MouseDefinition.cs
+++ Assets/Scripts/Entities/Definitions/MouseDefinition.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "entity/mouse",fileName = "MouseDefinition")]
 public class MouseDefinition : EntityDefinition
 {
+    public Rarity rarity;
 }
