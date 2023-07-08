658f2004b597ab7c60fffd5c2ee99e044e73124b
imberbus <littlelepetit@gmail.com>
2023-07-08 14:02:29 +0200

    Currency manager

---
+++ Assets/Scripts/Managers/Shop/CurrencyManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public int availableCurrency;
+
+    public void addCurrency(int lootedMoney)
+    {
+        availableCurrency += lootedMoney;
+    }
+
+    public bool hasEnoughCurrency(int requiredBudget)
+    {
+        return availableCurrency >= requiredBudget;
+    }
+
+    public bool useCurrency(int cost)
+    {
+        if (cost > availableCurrency)
+        {
+            Debug.Log("ALLO IL FAUT UTILISER HAS ENOUGH CURRENCY AVANT POUR EVITER DE S'ENDETTER");
+            return false;
+        }
+
+        availableCurrency -= cost;
+        return true;
+    }
+    
+}
