3aaa1ddc151e035da1dfea8fb681c459d4b06299
mvinet <mickaelvinet79@gmail.com>
2023-07-08 18:13:24 +0200

    RIP Little Mouse

--- Assets/Scripts/Entities/Mouse.cs
+++ Assets/Scripts/Entities/Mouse.cs
@@ -1,18 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Mouse : Entity<MouseDefinition>
 {
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private static readonly int Attack1 = Animator.StringToHash("Attack");
+    private static readonly int Death = Animator.StringToHash("Death");
 
     private void Start()
     {
         Init(definition);
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
         target = PlayStateManager.instance.cat;
     }
 
     private void FixedUpdate()
     {
+        
         transform.position = Vector3.MoveTowards(
             transform.position,
             target.transform.position,
@@ -23,6 +29,21 @@ public class Mouse : Entity<MouseDefinition>
     }
 
     public override void Die()
+    {
+        Debug.Log(name + " is dead, RIP IN PEACE LIttlE SOURIS");
+        _animator.ResetTrigger(Attack1);
+        _animator.SetTrigger(Death);
+    }
+
+    public override void Attack()
+    {
+        if(currentHp <= 0) return;
+        
+        base.Attack();
+        _animator.SetTrigger(Attack1);
+    }
+
+    public void OnDeathEvent()
     {
         Destroy(gameObject);
     }
